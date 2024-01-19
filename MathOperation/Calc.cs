7a2e6c4745c0c2e462d0e760f68b21f7c43f86da
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-20 01:05:01 +0200

    Add project files.

---
+++ MathOperation/Calc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperation
+{
+    internal class Calc
+    {
+        public int sum(int x , int y)
+        {
+            int result = x + y;
+            return result;
+        }    
+        public int minus(int x, int y)
+        {
+            int result = x-y;
+            return result;
+        }
+        public int mult(int x , int y)
+        {
+            int result = x * y;
+            return result;
+        }
+        public int mod(int x, int y)
+        {
+            int result = x % y;
+            return result;
+        }
+        public int min ( int x , int y)
+        {
+            if (x > y)
+            {
+                return y;
+            }
+            else
+            {
+                return x;
+            }
+        }
+        public int max (int x , int y)
+        {
+            if (x >y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
+        }
+        }
+    
+}
---
+++ MathOperation/Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MathOperation;
+using System.Runtime.CompilerServices;
+
+Calc c = new Calc();
+Console.WriteLine(" Choose the opertation ");
+Console.WriteLine(" a: Sum ");
+Console.WriteLine(" b: minus");
+Console.WriteLine(" c: Multiply");
+Console.WriteLine(" d: mod ");
+Console.WriteLine(" e: minimum");
+Console.WriteLine(" f: maximum");
+Console.WriteLine("           ");
+String choice = Console.ReadLine();
+if (choice == "a")
+{
+    Console.WriteLine("please enter first number");
+    String s1 = Console.ReadLine();
+    int num1 = Convert.ToInt32(s1);
+    Console.WriteLine("please enter second number");
+    String s2 = Console.ReadLine();
+    int num2 = Convert.ToInt32(s2);
+    int result = c.sum(num1, num2);
+    Console.WriteLine(" The sum " + num1 + " + " + num2 + " = " + result);
+}
+else if (choice == "b")
+{
+    Console.WriteLine("Enter the first number");
+    String s1 = Console.ReadLine();
+    int num1 = Convert.ToInt32(s1);
+    Console.WriteLine("Enter the second number");
+    String s2 = Console.ReadLine();
+    int num2 = Convert.ToInt32(s2);
+    int result = c.minus(num1, num2);
+    Console.WriteLine(" The sub of " + num1 + " - " + num2 + " = " + result);
+}
+else if (choice == "c")
+{
+    Console.WriteLine("Please enter first number");
+    String s1 = Console.ReadLine();
+    int num1 = Convert.ToInt32(s1);
+    Console.WriteLine("please enter first number");
+    String s2 = Console.ReadLine();
+    int num2 = Convert.ToInt32(s2);
+    int res = c.mult(num1, num2);
+    Console.WriteLine(" the mult of " + num1 + " * " + num2 + " = " + res);
+
+}
+else if (choice == "d")
+{
+    Console.WriteLine("Enter the first number");
+    String s1 = Console.ReadLine();
+    int num1 = Convert.ToInt32(s1);
+    Console.WriteLine(" Enter the second number");
+    String s2 = Console.ReadLine();
+    int num2 = Convert.ToInt32(s2);
+    int result = c.mod(num1, num2);
+    Console.WriteLine(" The mod of " + num1 + " % " + num2 + " = " + result);
+}
+else if ( choice== "e")
+{
+    Console.WriteLine("Please Enter the first number");
+    String s1 = Console.ReadLine();
+    int num1 = Convert.ToInt32(s1);
+    Console.WriteLine(" Please enter the second number");
+    String s2 = Console.ReadLine();
+    int num2 = Convert.ToInt32(s2);
+    int res = c.min(num1, num2);
+    Console.WriteLine(" the minus number between " + num1 + " and " + num2 + " is " + res);
+}
+else if (choice == "f")
+{
+    Console.WriteLine(" Enter the first number");
+    String s1 = Console.ReadLine();
+    int num1 = Convert.ToInt32(s1);
+    Console.WriteLine(" Please enter the second number");
+    String s2 = Console.ReadLine();
+    int num2 = Convert.ToInt32(s2);
+    int res = c.max(num1, num2);
+    Console.WriteLine(" the maximum of " + num1 + " and " + num2 + " is " + res);
+}
+else
+{
+    Console.WriteLine(" invalid choice ");
+}
+
+
